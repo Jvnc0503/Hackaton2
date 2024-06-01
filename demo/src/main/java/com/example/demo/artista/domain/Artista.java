0b53407b4aec7be156036aed62ad8066b5f0f784
Javier1308 <javier.olivares@utec.edu.pe>
2024-06-01 16:17:25 -0500

    Fix-Javier-Artista

--- demo/src/main/java/com/example/demo/artista/domain/Artista.java
+++ demo/src/main/java/com/example/demo/artista/domain/Artista.java
@@ -1,5 +1,9 @@
 package com.example.demo.artista.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
