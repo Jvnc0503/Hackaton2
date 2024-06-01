d8f507c75b4b1d0863e3d8219612c5324a8d65be
flauts <hector.nieto@utec.edu.pe>
2024-06-01 16:50:48 -0500

    awsd

--- demo/src/main/java/com/example/demo/listadereproduccion/infrastructure/ListaDeReproduccionRepository.java
+++ demo/src/main/java/com/example/demo/listadereproduccion/infrastructure/ListaDeReproduccionRepository.java
@@ -2,8 +2,13 @@ package com.example.demo.listadereproduccion.infrastructure;
 
 import com.example.demo.listadereproduccion.domain.ListaDeReproduccion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ListaDeReproduccionRepository extends JpaRepository<ListaDeReproduccion, Integer> {
-    List<ListaDeReproduccion> findAllByIdUser(int idUser);
+    @Query("select l from ListaDeReproduccion l " +
+            "where l.usuario.idUser = :userID")
+    List<ListaDeReproduccion> findAllByIdUser(@Param("userID") Integer userID);
 }
