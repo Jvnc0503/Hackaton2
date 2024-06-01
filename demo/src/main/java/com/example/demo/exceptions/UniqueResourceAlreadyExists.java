682306cbe427400aca7da4d2c6074505b16062c5
Jvnc0503 <jvnc0503@gmail.com>
2024-06-01 14:48:32 -0500

    GlobalExceptionHandler

--- demo/src/main/java/com/example/demo/exceptions/UniqueResourceAlreadyExists.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class UniqueResourceAlreadyExists extends RuntimeException {
-    public UniqueResourceAlreadyExists(String message) {
-        super(message);
-    }
-
-}
