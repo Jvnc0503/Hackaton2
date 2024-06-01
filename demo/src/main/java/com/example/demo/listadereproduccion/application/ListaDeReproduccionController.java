6c6bb8cbde66891de4aea86185d4bd970ad2842e
Ggburitox <thiago.rivera@utec.edu.pe>
2024-06-01 17:02:05 -0500

    a

--- demo/src/main/java/com/example/demo/listadereproduccion/application/ListaDeReproduccionController.java
+++ demo/src/main/java/com/example/demo/listadereproduccion/application/ListaDeReproduccionController.java
@@ -3,6 +3,7 @@ package com.example.demo.listadereproduccion.application;
 import com.example.demo.listadereproduccion.domain.ListaDeReproduccionService;
 import com.example.demo.listadereproduccion.dto.ListaDeReproduccionDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -16,27 +17,32 @@ public class ListaDeReproduccionController {
         this.listaDeReproduccionService = listaDeReproduccionService;
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/users/{idUser}/playlists")
     public ResponseEntity<List<ListaDeReproduccionDTO>> getUserPlaylists(@PathVariable Integer idUser) {
         return ResponseEntity.ok(listaDeReproduccionService.getUserPlaylists(idUser));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/playlists/{playlist_id}")
     public ResponseEntity<ListaDeReproduccionDTO> getPlaylistInfo(@PathVariable Integer playlist_id) {
         return ResponseEntity.ok(listaDeReproduccionService.getListaDeReproduccionInfo(playlist_id));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/users/{user_id}/playlists")
     public ResponseEntity<Void> createPlaylist(@PathVariable Integer user_id, @RequestBody ListaDeReproduccionDTO listaDeReproduccionDTO) {
         return ResponseEntity.created(URI.create(listaDeReproduccionService.createListaDeReproduccion(user_id, listaDeReproduccionDTO))).build();
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/playlists/{playlist_id}")
     public ResponseEntity<Void> updatePlaylist(@PathVariable Integer playlist_id, @RequestBody ListaDeReproduccionDTO listaDeReproduccionDTO) {
         listaDeReproduccionService.updateListaDeReproduccion(playlist_id, listaDeReproduccionDTO);
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/playlists/{playlist_id}")
     public ResponseEntity<Void> deletePlaylist(@PathVariable Integer playlist_id) {
         listaDeReproduccionService.deleteListaDeReproduccion(playlist_id);
